ad7d88541e9b7b6a00aa7ebc7789db5b3266a3b4
Jenna Martin <142928724+jenna-theresa01@users.noreply.github.com>
2024-03-04 10:19:53 -0500

    Update sum-without-highest-and-lowest-number.js

--- javascript/sum-without-highest-and-lowest-number.js
+++ javascript/sum-without-highest-and-lowest-number.js
@@ -7,3 +7,22 @@ function sumArray(array) {
   
   return sortedArray.slice(1, -1).reduce((sum, num) => sum + num, 0);
 }
+
+
+
+function sumArray(array) {
+  // check if the array is empty or has only one element
+  if(!array || array.length <= 1) {
+    return 0
+  }
+  
+  // sort the array in ascending order
+  array.sort(function(a, b) {
+    return a - b;
+  });
+  
+  // slice the array to exclude the first and last elements, then su the remaining elements
+  return array.slice(1, -1).reduce(function(sum, num) {
+    return sum + num;
+  }, 0);
+}
